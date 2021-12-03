82a29f844ff5794bc327fce86c251e743e6b2cfd
sriraghava2001 <71647938+sriraghava2001@users.noreply.github.com>
2021-12-03 10:47:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("a").innerHTML="How";
+document.writeln(5+"6");
+var x=5; 
+document.getElementById("a").innerHTML=Math.pow(x,2);
+ var x=120;
+var y="120";
+var z= x==y;
+document.write(z);
+document.getElementById("a").innerHTML=typeof z;
+var a=10;
+var b=20;
+var my={
+    firstName="Ragha",
+    lastName="vendra",
+    age:20
+};
+document.write(my);
+var x="My name is 'Raghava' ";
+document.write(x);
